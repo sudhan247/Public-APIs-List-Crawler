5d889988b6956267fa315f684540f41371965937
sudhan247 <56646285+sudhan247@users.noreply.github.com>
2020-06-16 11:25:25 +0530

    Update postman.py

--- postman.py
+++ postman.py
@@ -1,10 +1,10 @@
 import requests
 import json
 from time import *
-from collections import defaultdict
 import pandas as pd
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://user:pass@db',echo=True)
+from math import ceil
+engine = create_engine('oracle://system:1234@localhost',echo=True)
 class postman():
     def __init__(self):
         self.url=''
@@ -26,10 +26,9 @@ class postman():
             self.url = "https://public-apis-api.herokuapp.com/api/v1/apis/categories?page="+str(self.page_no)
             self.response = requests.request("GET", self.url, headers=self.headers, data = self.payload)
             self.retrieved_category=json.loads(self.response.text.encode('utf8'))['categories']
-            
-            if len(self.retrieved_category)==0:
-                break
             self.category.extend(self.retrieved_category)
+            if self.page_no==ceil(json.loads(self.response.text.encode('utf8'))['count']/10):
+                break
             self.page_no+=1
         print('Categories Retrieved:',len(self.category))
         print(self.category)
@@ -37,7 +36,6 @@ class postman():
         for i in range(len(self.category)):
             if '&' in self.category[i]:
                 self.category[i]=self.category[i].replace('&','%26')
-        
     def get_each_category(self):
         for page in range(self.ind,len(self.category)):
             category=self.category[page]
@@ -52,28 +50,27 @@ class postman():
                     self.retrieved_apis=json.loads(self.response.text.encode('utf8'))
                     self.retrieved_apis=self.retrieved_apis['categories']
                     self.all_api.extend(self.retrieved_apis)
-                    self.page_no+=1
-                    if len(self.retrieved_apis)==0:
+                    if self.page_no==ceil(json.loads(self.response.text.encode('utf8'))['count']/10):
                         self.ind+=1
                         break
+                    self.page_no+=1
                 except Exception as e:
                     if "error" in error.decode("utf-8"):
                         print('*'*50+'Token Expired'+'*'*50)
-                        sleep(35)
+                        sleep(38)
                         self.get_auth()
                         print('*'*49+'Token Activated'+'*'*49)
                     else:
                         print('*'*50+'Slept'+'*'*50)
-                        sleep(35)
+                        sleep(38)
                         print('*'*50+'Waked'+'*'*50)
         pd.DataFrame(self.all_api).to_sql('sudhan_postmanapi',
                                           con=engine,
                                           index=False
-                                          )
+                                          )        
 crawler=postman()
 crawler.get_all_categories()
 crawler.enc_all_categories()
 crawler.get_each_category()
 print(pd.DataFrame(engine.execute("select * from sudhan_postmanapi").fetchall(),
-                   columns=['API','Description','Auth','HTTPS','Cors','Link','Category']).to_string())
-
+                  columns=['API','Description','Auth','HTTPS','Cors','Link','Category']).to_string())
